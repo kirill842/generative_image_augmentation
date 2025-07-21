c23a6c00ce64fd9861c71d7a40df1282c5410945
Kirill <37930588+kirill842@users.noreply.github.com>
2025-07-21 22:24:21 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-PATH = "./inference_images_model_v10_ckpt_1445_photo_prompt"
-int(img_name.split(".")[0][-4:])
-resume = max([-1] + [int(img_name.split(".")[0][-4:]) for img_name in os.listdir(PATH)])
-print(resume+1)
